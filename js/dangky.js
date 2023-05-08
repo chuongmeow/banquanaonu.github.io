364dbea99ad4f80a3fd25196f739108f8655e27f
chuongmeow <21025231.chuong@student.iuh.edu.vn>
2023-05-08 17:29:22 +0700

    lan2

--- js/dangky.js
+++ js/dangky.js
@@ -1,15 +1,43 @@
-const form = document.getElementById('registerForm');
-form.addEventListener('submit', function(event) {
-  event.preventDefault();
-  const username = document.getElementById('username').value;
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
-  const confirmPassword = document.getElementById('confirmPassword').value;
-  if (username === '' || email === '' || password === '' || confirmPassword === '') {
-    alert('Please fill out all fields');
-  } else if (password !== confirmPassword) {
-    alert('Passwords do not match');
+// const form = document.getElementById('registerForm');
+// form.addEventListener('submit', function(event) {
+//   event.preventDefault();
+//   const username = document.getElementById('username').value;
+//   const email = document.getElementById('email').value;
+//   const password = document.getElementById('password').value;
+//   const confirmPassword = document.getElementById('confirmPassword').value;
+//   if (username === '' || email === '' || password === '' || confirmPassword === '') {
+//     alert('Please fill out all fields');
+//   } else if (password !== confirmPassword) {
+//     alert('Passwords do not match');
+//   } else {
+//     // Gửi yêu cầu đăng ký đến server ở đây
+//   }
+// });
+
+
+// validation form register and register user local storage
+const inputUsernameRegister = document.querySelector(".input-signup-username");
+const inputPasswordRegister = document.querySelector(".input-signup-password");
+const btnRegister = document.querySelector(".signup__signInButton");
+
+// validation form register and register user local storage
+
+btnRegister.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (
+    inputUsernameRegister.value === "" ||
+    inputPasswordRegister.value === ""
+  ) {
+    alert("vui lòng không để trống");
   } else {
-    // Gửi yêu cầu đăng ký đến server ở đây
+    // array user
+    const user = {
+      username: inputUsernameRegister.value,
+      password: inputPasswordRegister.value,
+    };
+    let json = JSON.stringify(user);
+    localStorage.setItem(inputUsernameRegister.value, json);
+    alert("Đăng Ký Thành Công");
+    window.location.href = "dangnhap.html";
   }
 });
--- js/dangnhap.js
+++ js/dangnhap.js
@@ -1,11 +1,39 @@
-const form = document.getElementById('loginForm');
-form.addEventListener('submit', function(event) {
-  event.preventDefault();
-  const username = document.getElementById('username').value;
-  const password = document.getElementById('password').value;
-  if (username === '' || password === '') {
-    alert('Please enter both username and password');
+// const form = document.getElementById('loginForm');
+// form.addEventListener('submit', function(event) {
+//   event.preventDefault();
+//   const username = document.getElementById('username').value;
+//   const password = document.getElementById('password').value;
+//   if (username === '' || password === '') {
+//     alert('Please enter both username and password');
+//   } else {
+//     // Gửi yêu cầu đăng nhập đến server ở đây
+//   }
+// });
+
+// validation form login
+const inputUsername = document.querySelector(".input-login-username");
+const inputPassword = document.querySelector(".input-login-password");
+const btnLogin = document.querySelector(".login__signInButton");
+
+// validation form login
+
+btnLogin.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (inputUsername.value === "" || inputPassword.value === "") {
+    alert("vui lòng không để trống");
   } else {
-    // Gửi yêu cầu đăng nhập đến server ở đây
+    const user = JSON.parse(localStorage.getItem(inputUsername.value));
+    
+    if (
+      user.username === inputUsername.value &&
+      user.password === inputPassword.value
+    ) {
+      alert("Đăng Nhập Thành Công");
+      window.location.href = "../index.html";
+    } else {
+      alert("Đăng Nhập Thất Bại");
+    }
   }
 });
+
+
