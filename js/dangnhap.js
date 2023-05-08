cdc07c6ea9f8405890a8aa792465a6a0ea0ce378
chuongmeow <21025231.chuong@student.iuh.edu.vn>
2023-05-08 20:23:30 +0700

    lan4

--- js/dangnhap.js
+++ js/dangnhap.js
@@ -1,14 +1,3 @@
-// const form = document.getElementById('loginForm');
-// form.addEventListener('submit', function(event) {
-//   event.preventDefault();
-//   const username = document.getElementById('username').value;
-//   const password = document.getElementById('password').value;
-//   if (username === '' || password === '') {
-//     alert('Please enter both username and password');
-//   } else {
-//     // Gửi yêu cầu đăng nhập đến server ở đây
-//   }
-// });
 
 // validation form login
 // const inputUsername = document.querySelector(".input-login-username");
